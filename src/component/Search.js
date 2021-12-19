3e7caf484970d1ca951ea1f7bbd3036757c831d3
trampham <pham1942000@gmail.com>
2021-12-19 15:00:08 +0700

    Demo algolia search

---
+++ src/component/Search.js
@@ -0,0 +1,36 @@
+import React from "react";
+import algoliasearch from "algoliasearch";
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  Stats
+} from "react-instantsearch-dom";
+const searchClient = algoliasearch(
+  "X0CHKXDR56",
+  "ae9103d10e61e753401f9d3f3f177c54"
+);
+
+console.log(searchClient)
+
+function Search() {
+  return (
+    <InstantSearch searchClient={searchClient} indexName="12">
+      <SearchBox/>
+      <div className="stats">
+        {" "}
+        <Stats />{" "}
+      </div>
+      <Hits hitComponent={Hit}/>
+    </InstantSearch>
+  );
+}
+
+const Hit =({hit}) =>
+<div>
+  <img src={hit.image}/>
+  <h3>{hit.name}</h3>
+  <br/>
+</div>
+
+export default Search;
